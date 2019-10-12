f41c7c83ec02ac953f69793bba63e6c81ccb6487
Mathew Adelmund <mathew.adelmund@ksd.org>
2019-10-12 14:13:29 -0700

    adding base project

---
+++ TopDownGameCollab/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMove : MonoBehaviour
+{
+    public float rotateSpeed = 2.0f;
+    public float moveSpeed = 5.0f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        transform.Rotate(new Vector3(0, 0, -x * rotateSpeed));
+        GetComponent<Rigidbody2D>().velocity = y * transform.up;
+
+    }
+}
